e455f1cb6c43f669fdc1accdaa532861581c0d7a
datquocngo <shaco14091999@gmail.com>
2019-03-13 10:58:19 +0700

    changed SecurityConfig

--- src/main/java/codegym/project/SecurityConfig.java
+++ src/main/java/codegym/project/SecurityConfig.java
@@ -10,11 +10,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/admins").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/admins")
                 .permitAll();
 
     }
